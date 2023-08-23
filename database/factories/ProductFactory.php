33e36fdad2e21832a796a7be1dcfbdd90cae9675
p738dev <pmech14@interia.pl>
2023-08-23 10:41:17 +0200

    SMB-1-Add ProductFactory file, update UserFactory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'id'=>$this->faker->uuid(),
+            'title'=>$this->faker->text(15, 20),
+            'desc'=>$this->faker->text(190),
+            'category'=>$this->faker->randomElement([ "sale",
+            "regular",
+            "newest",
+            "bargain"]),
+            'price'=>$this->faker->randomFloat(2, 100, 500),
+        ];
+    }
+}
