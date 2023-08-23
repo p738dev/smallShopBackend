a691dbe6493e1c0931a40e5d14bb78b538495f87
p738dev <pmech14@interia.pl>
2023-08-23 12:27:41 +0200

    SMB-3-Add routes for api project

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/logout', [UserController::class, 'logout']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::post('/orders', [OrderController::class, 'store']);   
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users/store', [UserController::class, 'store']);
+
+Route::middleware('auth:sanctum')->group(function(){
+        Route::post('/products', [ProductController::class, 'store']);
+        Route::put('/product_update/{id}', [ProductController::class, 'update']);
+        Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+        Route::get('/orders', [OrderController::class, 'index']);
+        Route::delete('/orders/{id}', [OrderController::class, 'destroy']); 
 });
