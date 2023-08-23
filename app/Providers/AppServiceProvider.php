f355a101e16e3b0d7739932e9bc6f7b7c053f59f
p738dev <pmech14@interia.pl>
2023-08-23 12:27:21 +0200

    SMB-3-Add providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,18 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\OrderRepository;
+use App\Repositories\ProductRepository;
+use App\Repositories\UserRepository;
+use App\Repositories\Contracts\OrderRepositoryInterface;
+use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Services\Contracts\OrderServiceInterface;
+use App\Services\Contracts\ProductServiceInterface;
+use App\Services\Contracts\UserServiceInterface;
+use App\Services\OrderService;
+use App\Services\ProductService;
+use App\Services\UserService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +26,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+
+        $this->app->bind(ProductServiceInterface::class, ProductService::class);
+
+        $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+
+        $this->app->bind(OrderServiceInterface::class, OrderService::class);
+        
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+
+        $this->app->bind(UserServiceInterface::class, UserService::class);
     }
 
     /**
