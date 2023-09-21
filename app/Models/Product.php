ee676cfd14a56bc19ddde55d503efe8a208f73d6
p738dev <pmech14@interia.pl>
2023-09-21 10:26:43 +0200

    SMB-4-Change Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,10 +10,6 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $primaryKey = 'id';
-    protected $keyType = 'string';
-    public $incrementing = false;
-
     protected $fillable = [
         'id',
         'title',
