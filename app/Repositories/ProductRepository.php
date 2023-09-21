009fe7d7ce47a8245409b3d4fb957c86737002fd
p738dev <pmech14@interia.pl>
2023-09-21 08:05:13 +0200

    SMB-3-Add queries in ProductRepository

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -14,12 +14,17 @@ class ProductRepository implements ProductRepositoryInterface
      * @return LengthAwarePaginator
      */
     public function index(Request $request) {
-        $search_param = $request->query('search');
+        $search_param = $request->query('searchParam');
+        $sort_param = $request->query('sortParam');
         $query = Product::query();
 
         if ($search_param) {
             $query->where('title', 'like', '%' . $search_param . '%');
         }
+        if($sort_param) {
+        $query->orderBy('price', strtoupper($sort_param));
+        }
+        
         $products = $query->paginate(10);
         return $products;
     } 
